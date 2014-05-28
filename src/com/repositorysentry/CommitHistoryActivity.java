97f29c8792b54123079df5a6431703c900bf3c27
floring <floring@users.noreply.github.com>
2014-05-28 18:23:27 +0300

    Refresh button added to commit history menu.

--- src/com/repositorysentry/CommitHistoryActivity.java
+++ src/com/repositorysentry/CommitHistoryActivity.java
@@ -6,6 +6,9 @@ import java.util.HashMap;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
@@ -19,14 +22,16 @@ public class CommitHistoryActivity extends ListActivity {
 		Intent intent = getIntent();
 		String username = intent.getStringExtra("Username");
 		String repositoryName = intent.getStringExtra("RepositoryName");
-		
+
 		CommitInspector inspector = CommitInspector.getInstance();
 		if (inspector.mDB == null) {
-			inspector.mDbHelper = new DatabaseOpenHelper(getApplicationContext());
+			inspector.mDbHelper = new DatabaseOpenHelper(
+					getApplicationContext());
 			inspector.mDB = inspector.mDbHelper.getWritableDatabase();
 		} else {
 			if (!inspector.mDB.isOpen()) {
-				inspector.mDbHelper = new DatabaseOpenHelper(getApplicationContext());
+				inspector.mDbHelper = new DatabaseOpenHelper(
+						getApplicationContext());
 				inspector.mDB = inspector.mDbHelper.getWritableDatabase();
 			}
 		}
@@ -43,5 +48,23 @@ public class CommitHistoryActivity extends ListActivity {
 						R.id.commitMessage });
 		ListView listView = (ListView) findViewById(android.R.id.list);
 		listView.setAdapter(adapter);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.refresh_history, menu);
+
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.action_refresh:
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
 	}
 }
